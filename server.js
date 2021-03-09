4c48618766fe7dbae4c0c3ec30e9abfa73df4cf3
Facuuzerbi <facuzerbinatti@gmail.com>
2021-03-09 14:59:44 -0300

    subiendo proyecto terminado

--- server.js
+++ server.js
@@ -7,16 +7,29 @@ mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
+// levantar la base de datos en Docker
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req,res)=> {
-    res.render('index');
+app.get('/', async (req,res)=> {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
 })
 
 app.post('/shortUrls', async (req, res) => {
     await ShortUrl.create({ full: req.body.fullUrl })
     res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req,res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if(shortUrl == null) return res.sendStatus(404);
+
+    shortUrl.clicks++
+    shortUrl.save()
+
+    res.redirect(shortUrl.full)
 })
 
 app.listen(process.env.PORT || 3000);
